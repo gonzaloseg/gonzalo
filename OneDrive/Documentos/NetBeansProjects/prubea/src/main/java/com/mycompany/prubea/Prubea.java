f00c70fe3d98c76d6d70745b3c7e66e4ede29f22
gondestrozavidas <gonzaloseg05@gmail.com>
2024-01-26 10:22:58 +0100

    hola cara cola

--- OneDrive/Documentos/NetBeansProjects/prubea/src/main/java/com/mycompany/prubea/Prubea.java
+++ OneDrive/Documentos/NetBeansProjects/prubea/src/main/java/com/mycompany/prubea/Prubea.java
@@ -1,3 +1,17 @@
-[user]
-      email=gonzaloseg05@gmail.com
-      name=gonzaloseg
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.prubea;
+
+/**
+ *
+ * @author gonza
+ */
+public class Prubea {
+
+    public static void main(String[] args) {
+       
+    }
+}
